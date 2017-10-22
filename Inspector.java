1118c102965dbf16905f76dc661986def7d60a71
Michael Radke <frederichenry070@gmail.com>
2017-10-22 16:28:49 -0600

    modifiers and fields seem to work, no arrays yet

--- Inspector.java
+++ Inspector.java
@@ -42,6 +42,80 @@ public static void inspect(Object obj, boolean recursive) throws Exception
 	System.out.println();		
 
 	
+	
+	
+	System.out.println("Methods in Class:" + '\n');
+	Method[] Methods = sentClass.getDeclaredMethods();
+	
+	for(int i = 0; i < (Methods.length); i++)
+	{
+		System.out.print(Methods[i].getName() + " THROWS: ");
+		
+		
+		
+		Class[] exceptions =  Methods[i].getExceptionTypes();
+		
+		
+		for(int j = 0; j < (exceptions.length); j++)
+		System.out.print(exceptions[j].getSimpleName() + ", ");
+		
+		
+		
+		
+		System.out.print("PARAMETERS: (");
+		Class[] parameters = Methods[i].getParameterTypes();
+		
+		for(int j = 0; j < (parameters.length); j++)
+			System.out.print(parameters[j].getSimpleName() + ", ");
+		
+		System.out.print(") ");
+		
+		
+		
+		System.out.print( "RETURNS: " + Methods[i].getReturnType().getSimpleName());
+		
+		
+		
+	
+		int modifier = Methods[i].getModifiers();
+		System.out.print(" MODIFIER NUMBER: " + modifier + " ");
+		
+		System.out.print( "MODIFIER: " + Modifier.toString(modifier));
+		
+		
+		
+		System.out.println();
+		
+	}
+	 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	System.out.println("FIELDS DECLARED by Class:");
+	Field []fields = sentClass.getDeclaredFields();
+	
+	
+	for(int i = 0; i< fields.length; i++)
+	{
+		
+		System.out.print("NAME: " + fields[i].getName());
+		System.out.print(" TYPE: " + fields[i].getType().toString());
+		System.out.print(" " + "MODIFIER NUMBER: " + fields[i].getModifiers());
+	    System.out.println(" MODIFIER: " + Modifier.toString(fields[i].getModifiers()));
+	}
+	
+	
+	
 }	
 	
 }
